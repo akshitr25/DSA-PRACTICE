1379b90125d260d8cf8026bda1d5693b4691be69
akshitr25 <akshitrathod1025@gmail.com>
2024-06-21 21:30:59 +0530

    DP

--- src/com/dsa/dp/DP10.java
+++ src/com/dsa/dp/DP10.java
@@ -1,6 +1,6 @@
 package com.dsa.dp;
 
-public class DP9 {
+public class DP10 {
 	public static void main(String as[])
 	{
 		//Taro's summer vacation starts tomorrow, and he has decided to make plans for it now.
@@ -17,18 +17,31 @@ public class DP9 {
 		//................
 		//if we dont know at each step that this option will give best answer now and also in future, then GREEDY/TWO POINTER
 		//ELSE, DP. best decision for future.
-		//P0, just find maxsum with 1 element from 1 array at any index. (GREEDY)
-//		int a[]= {10,40,70},n=a.length;
+		//P0, just find maxsum with 1 element from 1 array at any index.
+//		int a[]= {10,40,70},n=a.length; //actual way of taking i/p.
 //		int b[]= {20,50,80};
 //		int c[]= {30,60,90};
+		//3 dp arrays, dpa,dpb,dpc
 		int a[]= {10,20,30},n=a.length;
 		int b[]= {40,50,60};
 		int c[]= {70,80,90};
-		int dp[]=new int[n];
-		dp[0]=maxOf3(a[0],b[0],c[0]); //30,c
+		int dpa[]=new int[n];
+		int dpb[]=new int[n];
+		int dpc[]=new int[n];
+//		int dp[]=new int[n];
+		dpa[0]=a[0];
+		dpb[0]=b[0];
+		dpc[0]=c[0];
+		//dpa[i]=a[i]+Math.max(dpb[i-1],dpc[i-1]);
+		//dpb[i]=b[i]+Math.max(dpa[i-1],dpc[i-1]);
+		//dpc[i]=c[i]+Math.max(dpa[i-1],dpb[i-1]);
 		for(int i=1;i<n;i++)
-			dp[i]=maxOf3(a[i],b[i],c[i])+dp[i-1];
-		System.out.println(dp[n-1]);
+		{
+			dpa[i]=a[i]+Math.max(dpb[i-1],dpc[i-1]);
+			dpb[i]=b[i]+Math.max(dpa[i-1],dpc[i-1]);
+			dpc[i]=c[i]+Math.max(dpa[i-1],dpb[i-1]);
+		}
+		System.out.println(maxOf3(dpa[n-1],dpb[n-1],dpc[n-1]));
 	}
 	public static int maxOf3(int a,int b,int c)
 	{
