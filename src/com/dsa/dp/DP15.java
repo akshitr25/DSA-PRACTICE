22e9d633634c1530932743a627fa91f0be394eee
akshitr25 <akshitrathod1025@gmail.com>
2024-06-24 14:17:00 +0530

    DP

--- src/com/dsa/dp/DP15.java
+++ src/com/dsa/dp/DP15.java
@@ -8,13 +8,13 @@ public class DP15 {
 //		1) Subtract 1 from it. (n = n - ­1) ,
 //		2) If n is divisible by 2, divide by 2.( if n % 2 == 0, then n = n / 2 ) ,
 //		3) If n is divisible by 3, divide by 3. (if n % 3 == 0, then n = n / 3 ).
-		int n=10; //10,5,4,2,1
+		int n=10; //10,9,3,1
 		int dp[]=new int[n+1];
 		dp[1]=0;
 //		dp[2]=1;
 //		dp[3]=1;
 //		dp[4]=Math.min(dp[2],dp[3]+1);
-		for(int i=1;i<=n;i++)
+		for(int i=2;i<=n;i++)
 		{
 			int v1=dp[i-1]+1;
 			int v2=Integer.MAX_VALUE;
