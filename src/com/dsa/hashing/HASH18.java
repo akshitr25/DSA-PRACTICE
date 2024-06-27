4ca8511270a3f0851120e153c31d85222c835635
akshitr25 <akshitrathod1025@gmail.com>
2024-06-27 17:45:43 +0530

    Hash 20

--- src/com/dsa/hashing/HASH18.java
+++ src/com/dsa/hashing/HASH18.java
@@ -6,20 +6,32 @@ import java.util.Map;
 public class HASH18 {
 	public static void main(String as[])
 	{// hash 20
-
-		int a[]={1,4,-1,2},n=a.length,target=4,count=0; //1 based indexing
-		Map<Integer,Integer> map=new HashMap<>();
 //		Given an array of size “N” ; find the number of pairs (i,j) such that - 
 //		|a[i]-a[j]| + |a[i]+a[j]| = target 
 		//BF
+//		ai+aj,ai-aj,so 2ai=target && ai <aj
+//		ai+aj=target/2
+//		for every a[j] see numbers on the left which are lesser than it. i<j
+		int a[]={1,4,-1,2},n=a.length,target=4; //1 based indexing
+		Map<Integer,Integer> map=new HashMap<>();
+//		cl: less than target/2,ct2=no of target/2, and then no of pairs=cl*ct2. and then also add no of pairs of the occurrences of target/2 (n*(n-1)/2). only for target even
+//		target odd: 
+		int cl=0,ct2=0;
+		if(target%2!=0)
+		{
+			System.out.print(0);
+			return;
+		}
 		for(int i=0;i<n;i++)
 		{
-			for(int j=i+1;j<n;j++)
-			{
-				if(Math.abs(a[i]-a[j])+Math.abs(a[i]+a[j])==target)
-					count++;
-			}
+			map.put(a[i],map.getOrDefault(a[i],0)+1);
 		}
-		System.out.print(count);
+		for(int i=0;i<n;i++)
+		{
+			if(a[i]==target/2)	cl++;
+			else if(a[i]<target/2)	ct2++;
+		}
+		int ans=cl*ct2+cl*(cl-1)/2; //2*1+1*(0)/2=2
+		System.out.print(ans);
 	}
 }
