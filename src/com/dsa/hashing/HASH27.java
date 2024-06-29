f69716ca5b8f4b75efcbab212920fb4001c80bdc
akshitr25 <akshitrathod1025@gmail.com>
2024-06-29 14:48:26 +0530

    Hash 24

--- src/com/dsa/hashing/HASH27.java
+++ src/com/dsa/hashing/HASH27.java
@@ -10,31 +10,32 @@ public class HASH27 {
 //		OPTIMAL TC N^2, SC N
 		int a[]={4,5,3,1,2,4},n=a.length,target=13;
 //		int a[]= {1,5,3,1,2,10},target=20,n=a.length;
-		int count=0,ans=0;
-//		Map<Integer,Integer> map1=new HashMap<>();
+		int count=0;
 		int i,j,k,l;
+		Map<Integer,Integer> map=new HashMap<>();
 		for(i=0;i<n-1;i++)
 		{
-			for(j=i+1;j<n-2;j++)
+			for(j=i+1;j<n;j++)
 			{
-				int req=target-a[i]-a[j];
-				Map<Integer,Integer> map=new HashMap<>();
-				for(k=j+1;k<n;k++)
+				int temp=a[i]+a[j];
+				if(temp<target && map.containsKey(target-temp))
+					count+=map.get(target-temp);
+			}
+			for(j=0;j<i;j++)
+			{
+				int temp=a[i]+a[j];
+				if(temp<target)
 				{
-					if(map.containsKey(a[k]))
-						map.put(a[k],map.get(a[k])+1);
-					else
-						map.put(a[k],1);
+					if(map.containsKey(temp))
+						map.put(temp, map.get(temp)+1);
+					if(temp<target)
+					{
+						if(map.containsKey(temp))
+							map.put(temp,map.get(temp)+1);
+						else
+							map.put(temp, 1);
+					}
 				}
-				int count2x=0;
-				for(k=j+1;k<n;k++)
-				{
-					if(map.containsKey(req-a[k]))
-						count2x+=map.get(req-a[k]);
-					if(req-a[k]==a[k])	
-						count2x--;
-				}
-				count+=count2x/2;
 			}
 		}
 		System.out.println(count);
