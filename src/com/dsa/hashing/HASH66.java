94b22a394db08f9910f231f863ada5dc0516822e
akshitr25 <akshitrathod1025@gmail.com>
2024-07-08 14:53:02 +0530

    Hash 48

---
+++ src/com/dsa/hashing/HASH66.java
@@ -0,0 +1,19 @@
+package com.dsa.hashing;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HASH66 {
+	public static void main(String as[])
+	{// hash 48!
+//		Given a 2D matrix matrix, handle multiple queries of the following type:
+//Calculate the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+//Implement the NumMatrix class:
+	//NumMatrix(int[][] matrix) Initializes the object with the integer matrix matrix.
+	//int sumRegion(int row1, int col1, int row2, int col2) Returns the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+//You must design an algorithm where sumRegion works on O(1) time complexity.
+//		https://leetcode.com/problems/range-sum-query-2d-immutable/submissions/1135428876/
+	}
+}
