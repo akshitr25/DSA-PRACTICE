38100eec84122e5d93e721d3de8cdba326cdd488
akshitr25 <akshitrathod1025@gmail.com>
2024-07-08 17:01:08 +0530

    Hash 50

---
+++ src/com/dsa/hashing/HASH69.java
@@ -0,0 +1,18 @@
+package com.dsa.hashing;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HASH69 {
+	public static void main(String as[])
+	{// hash 50!
+//		GIVEN A MATRIX NxN, TAKE A SUBMATRIX AND FIND MAX NO OF POINTS WHICH CAN BE ACCOMODATED INSIDE IT.
+//		APPROACH:
+		//CONCEPT-1: CONVERT COORDINATES OF 2D MATRIX
+		//CONCEPT-2: FINDING PREFIX SUM OF 2D ARRAY
+		
+		System.out.println();
+	}
+}
