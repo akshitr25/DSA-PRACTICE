4d70d634a2d11a185d147c8d5197579c544c594b
akshitr25 <akshitrathod1025@gmail.com>
2024-07-15 11:25:01 +0530

    TP 11

---
+++ src/com/dsa/twopointer/TP14.java
@@ -0,0 +1,30 @@
+package com.dsa.twopointer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TP14 {
+	public static void main(String as[])
+	{//TP 11
+//		Longest subarray having sum of elements atmost K
+//		TC , SC 
+		int a[]={8,2,4,0,1,1,0},k=9;
+		//1,2,1,0,1,1,0 | 4 | ans=5
+		int n=a.length,maxlen=-1,i=0,j=0,sum=0;
+		while(j<n)
+		{
+			sum+=a[j];
+			if(sum<=k)
+			{
+				maxlen=Math.max(maxlen,j-i+1);
+			}
+			else //if(sum>k) STOP
+			{
+				sum-=a[i];
+				i++;
+			}
+			j++;
+		}
+		System.out.println(maxlen);
+	}
+}
