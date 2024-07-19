12e15ae676e4b16bacc1cea566d7ae85dbaaaf99
akshitr25 <akshitrathod1025@gmail.com>
2024-07-19 17:03:12 +0530

    BS 8

--- src/com/dsa/binarysearch/BS9.java
+++ src/com/dsa/binarysearch/BS9.java
@@ -1,26 +1,41 @@
 package com.dsa.binarysearch;
 
-public class BS8 {
+public class BS9 {
 	public static void main(String as[])
 	{//BS 8
 //		You are given an array of size “N” ; you are given a capacity of “k” ; 
 //		Each element can be reduced by at most k in 1 move. You are allowed to make maximum “d” moves. Find the minimum possible value of “k” 
-//		BF TC N*limit, SC 1
+//		BF TC N*log(limit), SC 1
 		int a[]={2,4,3},k=3,d=4;
 //		int a[]={2,3,4,5},k=1,d=5;
 		int n=a.length,ans=0;
 		int limit=-1;
 		for(int i:a)
 			limit=Math.max(i,limit);
-		while(k<=limit)
+		int low=1,high=limit;
+		while(low<=high)
 		{
-			if(check(k,a,d)==false)
-				k++;
+			int mid=(low+high)/2;
+			if(check(mid,a,d)==false)
+			{
+				low=mid+1;
+			}
 			else
 			{
-				ans=k;
-				break;
+				if(mid==1)
+				{
+					ans=mid;
+					break;
+				}
+				else if(check(mid-1,a,d)==false)
+				{
+					ans=mid;
+					break;
+				}
+				else
+					high=mid-1;
 			}
+				
 		}
 		System.out.println(ans);
 	}
