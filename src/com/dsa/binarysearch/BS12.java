327c678fcdf46c59ea9c8eb22fda3badec9fbe3a
akshitr25 <akshitrathod1025@gmail.com>
2024-07-20 19:26:59 +0530

    BS 11 and 12

--- src/com/dsa/binarysearch/BS12.java
+++ src/com/dsa/binarysearch/BS12.java
@@ -7,35 +7,30 @@ import java.util.List;
 public class BS12 {
 	public static void main(String as[])
 	{//BS 11
-//		Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
-//		You must write an algorithm with O(log n) runtime complexity.
-		int a[]={1,2,3,5,6,7,8,9,10},k=4;
-		System.out.println(searchInsert(a,k));
+//		You are given an array of characters  that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+//Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+//		
+		char a[]={'c','f','j'},k='e';
+		System.out.println(nextGreatestLetter(a,k));
     }
-	static int searchInsert(int[] a, int k) {
-        int n=a.length,left=0,right=n-1,mid=-1;
-        if(k>a[right])  return n;
+	static char nextGreatestLetter(char[] a, char k) {
+        int n=a.length,left=0,right=n-1;
         while(left<=right)
         {
-            mid=(left+right)/2;
-            if(a[mid]==k)
-            {
-                return mid;
-            }
-            else if(a[mid]>k)
-            {
-                if(mid>0 && a[mid-1]<k)
-                {
-                    return mid;
-                }
-                else 
-                    right=mid-1;
-            }
-            else if(a[mid]<k)
+            int mid=(left+right)/2;
+            char c=a[mid];
+            if(c<=k)
             {
                 left=mid+1;
             }
+            else if(c>k)
+            {
+                if(mid>0 && a[mid-1]>k)
+                    right=mid-1;
+                else
+                    return a[mid];
+            }
         }
-        return mid;
+        return a[0];
     }
 }
---
+++ src/com/dsa/binarysearch/BS13.java
@@ -0,0 +1,41 @@
+package com.dsa.binarysearch;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BS13 {
+	public static void main(String as[])
+	{//BS 12
+//		Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+//		You must write an algorithm with O(log n) runtime complexity.
+		int a[]={1,2,3,5,6,7,8,9,10},k=4;
+		System.out.println(searchInsert(a,k));
+    }
+	static int searchInsert(int[] a, int k) {
+        int n=a.length,left=0,right=n-1,mid=-1;
+        if(k>a[right])  return n;
+        while(left<=right)
+        {
+            mid=(left+right)/2;
+            if(a[mid]==k)
+            {
+                return mid;
+            }
+            else if(a[mid]>k)
+            {
+                if(mid>0 && a[mid-1]<k)
+                {
+                    return mid;
+                }
+                else 
+                    right=mid-1;
+            }
+            else if(a[mid]<k)
+            {
+                left=mid+1;
+            }
+        }
+        return mid;
+    }
+}
